7604c49e37420f5ce261d4e50c85680241ad454e
GaelR12 <gaelr1830@gmail.com>
2022-09-27 14:17:31 -0500

    class-10

---
+++ class-10/exercise-5/text.js
@@ -0,0 +1,16 @@
+//Number
+let length = 16;
+//String
+let lastname = "Rueda";
+//Object
+let x = {FirstName: "Gael, lastName:Rueda"};
+//Boolean
+let y = 12;
+let a = 22;
+let z = 6;
+//Array
+
+console.log(length)
+console.log(lastname)
+console.log(x)
+console.log(a)
