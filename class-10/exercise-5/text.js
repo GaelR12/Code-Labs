bf8b95a93f3951a2ecc0170fbf217dfea3005039
GaelR12 <gaelr1830@gmail.com>
2022-09-28 14:21:09 -0500

    class-10

--- class-10/exercise-5/text.js
+++ class-10/exercise-5/text.js
@@ -9,8 +9,13 @@ let y = 12;
 let a = 22;
 let z = 6;
 //Array
+const food = ["Hamburger", "Hotdogs", "Nachos"];
+
 
 console.log(length)
 console.log(lastname)
 console.log(x)
 console.log(a)
+console.log(y == a)
+console.log(z == z)
+console.log(food)
---
+++ class-10/exercise-6/text.js
@@ -0,0 +1,4 @@
+console.log(firstName)
+console.log(lastName)
+console.log (favoriteNumber)
+console.log(person)
