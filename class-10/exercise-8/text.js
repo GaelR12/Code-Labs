74c3f8e3467063a68fe7b66ab8a1f53f8aec8d6d
GaelR12 <gaelr1830@gmail.com>
2022-09-29 14:23:23 -0500

    class-10

---
+++ class-10/exercise-8/text.js
@@ -0,0 +1,15 @@
+let a = 5;
+let b = 5; 
+let c = (a == b);
+// equal value and equal type
+let g = (5 == 5);
+//not equal value or not equal type
+let k = (5 !== 5);
+// greater then 
+let p = (5 > 5);//false
+//less than
+let q = (5 < 10);//true
+// greater than or equal to
+let r = (5 >= 5);
+//less than or equal to
+let s = (5 <= 5);//true
