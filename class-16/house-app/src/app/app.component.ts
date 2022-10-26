fa8c17c5ece551025bd88cd7937f33946e8e7f02
GaelR12 <gaelr1830@gmail.com>
2022-10-26 14:19:58 -0500

    class-16

--- class-16/house-app/src/app/app.component.ts
+++ class-16/house-app/src/app/app.component.ts
@@ -3,7 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: '<app-server></app-server><app-server></app-server>',
-  styleUrls: ['./app.component.css']
+  //styleUrls: ['./app.component.css']
+  styles: ['
+  h1 {
+color:rgb(255, 0, 0)
+}
+']
+
 })
 export class AppComponent {
   title = 'house-app';
