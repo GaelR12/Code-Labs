f2be9d49efe1d8c61d0c6edcd49a41f892820aad
GaelR12 <gaelr1830@gmail.com>
2022-11-14 13:47:51 -0600

    Managing recipe exercise

--- class-10/Code-Along/code-along/src/app/app.module.ts
+++ class-10/Code-Along/code-along/src/app/app.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {RecipeService} from './recipes';                                                       }
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
     AppComponent
+    providers: [RecipeService ]
   ],
   imports: [
     BrowserModule
--- class-10/Code-Along/code-along/src/assets/shopping/shopping.list.service.ts
+++ class-10/Code-Along/code-along/src/assets/shopping/shopping.list.service.ts
@@ -1,3 +1,11 @@
+import { Recipe } from './recipe.model';
 export class ShoppingListService {
+private recipes: Recipe[] = [
+  new Recipe (test)
+  new Recipe (test)
+];
 
+get Recipes(){
+  return.this.recipes.slice();
+}
 }
